7f168cf9334fd3fd9577a0c8b559a578dbf2d179
Daniel Acquesta <danielacquesta@sbcglobal.net>
2018-02-11 14:39:02 -0800

    created select component

--- src/components/my-picks-page.js
+++ src/components/my-picks-page.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import UserNav from './user-nav';
+import MyPicksForm from './my-picks-form';
 
 export function MyPicksPage() {
   return (
     <div>
       <UserNav />
       <h1>My Picks Page</h1>
+      <MyPicksForm />
     </div>
   );
 }
---
+++ src/components/select.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default class Select extends React.Compoennt {
+  render() {
+    const options = this.props.options.map((option) => (
+      <option key={option} value={option}>{option}</option>
+    ));
+    const {input, name} = this.props;
+    return (
+      <div>
+        <select {...input} {...name}>
+          <option value=""></option>
+          {options}
+        </select>
+      </div>
+    );
+  }
+}
