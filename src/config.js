d93071c34aa8b39012762eb27028876a42abe61a
Daniel Acquesta <danielacquesta@sbcglobal.net>
2018-03-01 14:57:18 -0800

    updated config file

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 export const API_BASE_URL =
-process.env.REACT_APP_API_BASE_URL || 'http://localhost:8080/';
+process.env.REACT_APP_API_BASE_URL || 'http://localhost:8080';
 //variable names have to start with REACT_APP_ with create-react-app
