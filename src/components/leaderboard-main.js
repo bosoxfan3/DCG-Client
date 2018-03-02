4a7da45c433d7782e71b2c8e6f0b3ecf66637a87
Daniel Acquesta <danielacquesta@sbcglobal.net>
2018-03-02 11:15:55 -0800

    testing leaderboard styling on mobile

--- src/components/leaderboard-main.js
+++ src/components/leaderboard-main.js
@@ -8,13 +8,13 @@ export class LeaderboardMain extends React.Component {
     const allUsers = this.props.allUsers.map((user, index) => {
       return (
         <div className="rankings" key={index}>
-          <div className="col-1">
+          <div className="rank col-1">
             <h3>{index+1}</h3>
           </div>
-          <div className="col-4">
+          <div className="entry col-4">
             <h3>{user.name}</h3>
           </div>
-          <div className="col-4">
+          <div className="user col-4">
             <h3>{user.username}</h3>
           </div>
           <div className="col-3">
@@ -28,14 +28,14 @@ export class LeaderboardMain extends React.Component {
         <div className="col-1">
           <h2>Rank</h2>
         </div>
-        <div className="col-4">
+        <div className="entry-name col-4">
           <h2>Entry Name</h2>
         </div>
-        <div className="col-4">
+        <div className="username col-4">
           <h2>Username</h2>
         </div>
         <div className="col-3">
-          <h2>Total Points</h2>
+          <h2>Points</h2>
         </div>
       </div>
     )
