4d0d3bc8a7e3f902fb34bbff6d58f2aa527e0e63
Daniel Acquesta <danielacquesta@sbcglobal.net>
2018-03-14 20:31:11 -0700

    created tests folder and moved app.test to it

--- src/__tests__/app.test.js
+++ src/__tests__/app.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { shallow } from 'enzyme';
 
-import App from './app';
+import App from '../components/app';
 
 describe('<App />', () => {
   it('renders without crashing', () => {
