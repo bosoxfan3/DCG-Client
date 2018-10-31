af506cc202a05b7df7adb805e67d4f2a202a72ce
Daniel Acquesta <danielacquesta@sbcglobal.net>
2018-10-31 10:33:20 -0700

    changed for week 9

--- src/components/pickem/my-picks-main.js
+++ src/components/pickem/my-picks-main.js
@@ -27,7 +27,7 @@ export class MyPicksMain extends React.Component {
         }
     }
     render() {
-        const lockoutTime = new Date('21 October 2018 10:00:00');
+        const lockoutTime = new Date('4 November 2018 10:00:00');
         const matchups = this.props.matchupData.map((matchup, index) => {
             return (
                 <div className="matchup" key={index}>
@@ -53,7 +53,7 @@ export class MyPicksMain extends React.Component {
             <div id="my-picks-main">
                 <div className="row">
                     <div className="main-area col-12">
-                        <h1>Week 7</h1>
+                        <h1>Week 9</h1>
                         <section className="col-3">
                             <form onSubmit={this.props.handleSubmit(values => this.onSubmit(values))}>
                                 {matchupSelects}
